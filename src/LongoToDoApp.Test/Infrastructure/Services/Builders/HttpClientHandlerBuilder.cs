04ce1dc15415ee06f1f0cb6ae26870bcdccd6cec
Enrique Largacha Gil <enrique.largacha@outlook.com>
2023-06-23 01:55:39 +0200

    Added ToDo items service unit test

---
+++ src/LongoToDoApp.Test/Infrastructure/Services/Builders/HttpClientHandlerBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using Moq.Protected;
+
+namespace LongoToDoApp.Test.Infrastructure.Services.Builders
+{
+	public static class HttpClientHandlerBuilder
+	{
+		public static HttpClientHandler WithResponse(HttpResponseMessage response)
+		{
+			var handler = new Mock<HttpClientHandler>();
+
+			handler.Protected()
+				.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(response)
+				.Verifiable();
+
+			return handler.Object;
+		}
+	}
+}
