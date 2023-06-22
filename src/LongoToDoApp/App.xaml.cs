88a8230f663bab82fb540120af43267c1ab36205
Enrique Largacha Gil <enrique.largacha@outlook.com>
2023-06-22 15:54:23 +0200

    Added src project

---
+++ src/LongoToDoApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LongoToDoApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/LongoToDoApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LongoToDoApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
